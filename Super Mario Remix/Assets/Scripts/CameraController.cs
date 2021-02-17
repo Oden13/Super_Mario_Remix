eec5c5f3d33625de00a7bf5ae85ed443592e7314
roneelee522 <roneelee522@hotmail.com>
2021-02-16 20:54:08 -0500

    Sample level added

---
+++ Super Mario Remix/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float moveSpeed = 100f;
+    public Transform target; 
+
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, moveSpeed * Time.deltaTime);
+    }
+}
