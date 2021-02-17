e9d17dd56cc4393597edd26b498b56ed274256ad
Peyton Paul <peyton.z.paul@gmail.com>
2021-02-17 11:38:36 -0500

    Working on Main Menu

---
+++ Super Mario Remix/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SwitchScene(string Main)
+{
+        SceneManager.LoadScene("Main");
+    }
+    public void quitgame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
